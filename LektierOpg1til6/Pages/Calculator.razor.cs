673f83d0970e3b6e0a8d8f6e037ca3ad2a2dc789
Caroline <caroline@groth-clausen.dk>
2025-09-28 15:41:16 +0200

    Add project files.

---
+++ LektierOpg1til6/Pages/Calculator.razor.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LektierOpg1til6.Pages
+{
+    public partial class Calculator
+    {
+        public double number1 = 0;
+        public double number2 = 0;
+        public double total = 0;
+
+        public void CalculateAdd() => total = number1 + number2;
+        public void CalculateMinus() => total = number1 - number2;
+        public void CalculateGange() => total = number1*number2;
+        public void CalculateDiv()
+        {
+            total=number1/number2;
+        }
+
+        public void CalculateFakultet()
+        {
+            total =fakultet(number1);
+        }
+
+        private double fakultet(double n)
+        {
+            if (n < 0)
+              return 0;
+            double result = 1;
+           
+            for (double i = 2; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
+
