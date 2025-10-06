93f0902907c6ad169c824a39290d05b41316715f
Caroline <caroline@groth-clausen.dk>
2025-10-06 13:50:57 +0200

    Virker før dropdown

--- LektierOpg1til6/Pages/ProductPage.razor.cs
+++ LektierOpg1til6/Pages/ProductPage.razor.cs
@@ -1,11 +1,15 @@
 ﻿using LektierOpg1til6.Models;
-using Microsoft.AspNetCore.Components.Forms;  
+using Microsoft.AspNetCore.Components.Forms;
 namespace LektierOpg1til6.Pages
 {
     public partial class ProductPage
     {
         private Product ProductModel = new Product();
         private EditContext EditContext;
+        private string? StatusMessage;
+        private string AlertClass = "alert info";
+        
+        private List<Product> Products = new List<Product>();
 
         protected override void OnInitialized()
         {
@@ -14,17 +18,32 @@ namespace LektierOpg1til6.Pages
 
         private void HandleValidSubmit()
         {
-            Console.WriteLine("HandleValidSubmit Called...");
+           StatusMessage = "Product saved successfully!";
+            
+            Products.Add(new Product
+            {
+                Name = ProductModel.Name,
+                Price = ProductModel.Price,
+                PublishedDate = ProductModel.PublishedDate,
+                Description = ProductModel.Description,
+                IsPublished = ProductModel.IsPublished
+            });
 
+            //Nulstil model og EditContext
             ProductModel = new Product();
             EditContext = new EditContext(ProductModel);
         }
 
         private void HandleInvalidSubmit()
         {
-            Console.WriteLine("HandleInvalidSubmit Called...");
+            StatusMessage = "There are some validation errors. Please try again.";
+            AlertClass = "alert error";
         }
-
-
+        private void ClearProducts()
+        {
+            Products.Clear();
+            StatusMessage = "Produktlisten er ryddet.";
+            AlertClass = "alert-info";
+        }
     }
 }
